f97628675bd31515fa05bb1fcf992ee9fe84041e
Akhil <ak007@gmail.com>
2023-10-12 11:29:54 +0530

    change in nodemailer file

--- helper/nodemailer.js
+++ helper/nodemailer.js
@@ -1,41 +1,38 @@
-const nodemailer = require('nodemailer');
-const path = require('path');
+const nodemailer = require("nodemailer");
+const path = require("path");
 
 module.exports = {
-    sendEmail: async (email, subject, html) => {
-        return new Promise(async resolve => {
-            let transporter = nodemailer.createTransport({
-                host: "smtp.gmail.com",//or "gmail"
-                port: 465,//optional
-                secure: true,//optional
-                auth: {
-                    user: "test.user25112020@gmail.com",
-                    pass: "utxjyaogndaroqdz"
-                }
-            });
-            var mailOptions = {
-                from: "test.user25112020@gmail.com",
-                to: email,
-                subject: subject,
-                html: html
-            };
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.log("error" + error)
-                    return resolve({ status: false, data: [], message: 'Could not send Email!' });
-                }
-                console.log("info " + info)
-                console.log('Message sent: %s', info.messageId);
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-                return resolve({ status: true, data: [], message: 'OTP sent!.' });
-            });
-        });
-    }
-}
-
-
-
-
-
-
-
+  sendEmail: async (email, subject, html) => {
+    return new Promise(async (resolve) => {
+      let transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_AUTH_USER,
+          pass: process.env.EMAIL_AUTH_PASS,
+        },
+      });
+      var mailOptions = {
+        from: "test.user25112020@gmail.com",
+        to: email,
+        subject: subject,
+        html: html,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("error" + error);
+          return resolve({
+            status: false,
+            data: [],
+            message: "Could not send Email!",
+          });
+        }
+        console.log("info " + info);
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        return resolve({ status: true, data: [], message: "OTP sent!." });
+      });
+    });
+  },
+};
